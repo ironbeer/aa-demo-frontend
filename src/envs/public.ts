7ac21453bac1940fad77402dceb5664869ae2c83
ironbeer <7997273+ironbeer@users.noreply.github.com>
2025-03-19 04:03:53 +0900

    デプロイのためにコミット

--- src/envs/public.ts
+++ src/envs/public.ts
@@ -4,39 +4,40 @@
 
 import type { Address } from "viem";
 
-const $ = process.env;
-
 // Human-readable title for your website
 export const rpName =
-  $.NEXT_PUBLIC_RP_NAME || "AccountAbstraction demo with WebAuthn";
+  process.env.NEXT_PUBLIC_RP_NAME || "AccountAbstraction demo with WebAuthn";
 
 // A unique identifier for your website. 'localhost' is okay for local dev
-export const rpID = $.NEXT_PUBLIC_WEBAUTHN_RP_ID || "localhost";
+export const rpID = process.env.NEXT_PUBLIC_WEBAUTHN_RP_ID || "localhost";
 
 // The URL at which registrations and authentications should occur.
 // 'http://localhost' and 'http://localhost:PORT' are also valid.
 // Do NOT include any trailing /
 export const rpOrigin =
-  $.NEXT_PUBLIC_WEBAUTHN_ORIGIN || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_WEBAUTHN_ORIGIN || "http://localhost:3000";
 
 // ブロックチェーンのRPCエンドポイント
-export const rpcURL = $.NEXT_PUBLIC_RPC_ENDPOINT || "http://localhost:8545";
+export const rpcURL =
+  process.env.NEXT_PUBLIC_RPC_ENDPOINT || "http://localhost:8545";
 
 // EntryPointコントラクトのアドレス
-export const entryPointAddress = $.NEXT_PUBLIC_AA_ENTRY_POINT as Address;
+export const entryPointAddress = process.env
+  .NEXT_PUBLIC_AA_ENTRY_POINT as Address;
 
 // CoinbaseSmartWalletFactoryのアドレス
-export const coinbaseSmartWalletFactoryAddress =
-  $.NEXT_PUBLIC_COINBASE_SMART_WALLET_FACTORY as Address;
+export const coinbaseSmartWalletFactoryAddress = process.env
+  .NEXT_PUBLIC_COINBASE_SMART_WALLET_FACTORY as Address;
 
 // Paymasterトークンアドレス
-export const paymasterAddress = $.NEXT_PUBLIC_PAYMASTER as Address;
+export const paymasterAddress = process.env.NEXT_PUBLIC_PAYMASTER as Address;
 
 // トランザクション送信に使用するアカウント
-export const bundlerAddress = $.NEXT_PUBLIC_BUNDLER_ADDRESS as Address;
+export const bundlerAddress = process.env
+  .NEXT_PUBLIC_BUNDLER_ADDRESS as Address;
 
 // エクスプローラURL
-export const explorerURL = $.NEXT_PUBLIC_EXPLORER_URL as string;
+export const explorerURL = process.env.NEXT_PUBLIC_EXPLORER_URL as string;
 
 // PaymasterのエクスプローラURL
 export const paymasterURL = `${explorerURL}/address/${paymasterAddress}`;
--- src/envs/server.ts
+++ src/envs/server.ts
@@ -5,7 +5,5 @@
 import type { Hex } from "viem";
 export * from "./public";
 
-const $ = process.env;
-
 // トランザクション送信に使用するアカウント
-export const bundlerPrivateKey = $.BUNDLER_PRIVATE_KEY as Hex;
+export const bundlerPrivateKey = process.env.BUNDLER_PRIVATE_KEY as Hex;
